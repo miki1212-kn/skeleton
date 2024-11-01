260456269c016350ae034a7f7b220543ab19ad76
mk <kanzakimiki.12@gmail.com>
2024-11-01 15:19:04 +0900

    fullcalender create

---
+++ src/app/calender/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import TestCalender from "../components/TestCalender";
+
+const CalenderPage: React.FC = () => {
+  return (
+    <div>
+      <h1>カレンダー</h1>
+      <TestCalender />
+    </div>
+  );
+};
+
+export default CalenderPage;
