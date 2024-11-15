b5b35d600c70687a350a32c40c8a0bb45f97ebb0
tw234-jpg <103188290+tw234-jpg@users.noreply.github.com>
2024-11-15 16:09:45 +0900

    編集

--- src/app/calender/page.tsx
+++ src/app/calender/page.tsx
@@ -5,7 +5,7 @@ import TestCalender from "../components/TestCalender";
 const CalenderPage: React.FC = () => {
   return (
     <div>
-      <h1>カレンダー</h1>
+
       <TestCalender />
     </div>
   );
