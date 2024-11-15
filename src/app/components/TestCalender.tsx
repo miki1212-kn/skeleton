d679d0c1389aa10da03905632f6e48380e3024bd
mk <kanzakimiki.12@gmail.com>
2024-11-15 18:06:57 +0900

    5行表示と6行表示
    レンダリングしてみたけど、なんか無理
    明日やる
    今エラーなし

--- src/app/components/TestCalender.tsx
+++ src/app/components/TestCalender.tsx
@@ -1,19 +1,28 @@
 "use client";
 
 import React from "react";
+import { useEffect, useState } from "react";
 
 //fullcalenderからのimport
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
-import interactionPlugin from "@fullcalendar/interaction";
+// import ja from "../locales/ja";a
 
-//data-fns
-import { format } from "date-fns";
-
+import styles from "./MainCalender.module.scss";
 //日付をクリックした際にその日付をフォーマットとして表示する関数コンポーネント
+
+import { format } from "date-fns";
 //React.FCでpropsを受け取れる
 const TestCalender: React.FC = () => {
+  const [isSixRows, setIsSixRows] = useState(false);
+
+  // カレンダーの行数を判定
+  const handleDatesSet = () => {
+    const weeks = document.querySelectorAll(".fc-daygrid-week");
+    setIsSixRows(weeks.length === 6); // 行数が6の場合は true に設定
+  };
+
   //argという引数を受け取る関数
 
   const handleDateClick = (arg: any) => {
@@ -21,17 +30,28 @@ const TestCalender: React.FC = () => {
   };
 
   return (
-    <FullCalendar
-      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-      initialView="dayGridMonth"
-      editable={true}
-      selectable={true}
-      dateClick={handleDateClick}
-      events={[
-        { title: "予定１", date: "2024-11-01" },
-        { title: "予定2", date: "2024-11-03" },
-      ]}
-    />
+    <div
+      className={`${styles.calenderContainer} ${
+        isSixRows ? styles.sixRows : styles.fiveRows
+      }`}
+    >
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin]}
+        initialView="dayGridMonth"
+        editable={true} //イベントのドラッグ操作を許可
+        selectable={true} //日付選択を許可
+        dateClick={handleDateClick} //日付クリックのイベントハンドラ
+        businessHours={true}
+        // showNonCurrentDates={false}
+        datesSet={handleDatesSet}
+        // height="660px"
+        // locale={ja}
+        events={[
+          { title: "予定１", date: "2024-11-01" },
+          { title: "予定2", date: "2024-11-03" },
+        ]}
+      />
+    </div>
   );
 };
 
